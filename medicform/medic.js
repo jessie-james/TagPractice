33844e5f45d579b5315c79c94aded14dc88406e6
jessie-james <jess.jamestwoo11@gmail.com>
2019-05-20 19:46:47 -0600

    project complete

---
+++ medicform/medic.js
@@ -0,0 +1,22 @@
+function myFunction() {
+  var myName = "Grant";
+  if (myName === "Grant") {
+    alert("Submission Failed due to error VERN");
+  } else if (myName == !"Jessie") {
+    console.log("that is definatly not Jessie");
+  } else {
+    console.log("Thats not Grant!");
+  }
+}
+
+function myFor() {
+  for (var i = 1; i <= 7; i++) {
+    alert(
+      "You have just entered a new dimension welcome to the " + i + " dimension"
+    );
+  }
+}
+
+
+//Strings Numbers Booleans Arrays(Why they are useful ? What does “0 - indexed” mean ?)
+
